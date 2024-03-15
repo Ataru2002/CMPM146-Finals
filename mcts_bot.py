4a9bb6bd8d80e06f3a2bcd3c2ccde602bb80c6de
gavilori <gavilori@ucsc.edu>
2024-03-15 14:19:02 -0700

    set up MCTS framework

---
+++ mcts_bot.py
@@ -0,0 +1,28 @@
+from mcts_bot import MCTSNode
+from gameMaze import gameMaze
+import constants
+import random
+
+num_nodes = 100
+explore_faction = 2.
+
+def traverseNodes(node: MCTSNode, maze: gameMaze, state):
+    pass
+
+def expandLeaf(node: MCTSNode, maze: gameMaze, state):
+    pass
+
+def rollout(maze: gameMaze, state):
+    pass
+
+def backpropagate(node: MCTSNode, won: bool):
+    pass
+
+def ucb(node: MCTSNode):
+    pass
+
+def getBestAction(root: MCTSNode):
+    pass
+
+def think(maze: gameMaze, currentState):
+    pass
